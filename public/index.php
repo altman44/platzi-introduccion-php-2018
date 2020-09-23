ab4723d785b4851212210923f33251e1085656e3
altman.nicolas44@gmail.com <altman.nicolas44@gmail.com>
2020-09-23 17:59:47 -0300

    Modified environment variables

--- public/index.php
+++ public/index.php
@@ -22,9 +22,9 @@ $capsule = new Capsule;
 $capsule->addConnection([
     'driver'    => getenv('DB_DRIVER'),
     'host'      => getenv('DB_HOST'),
-    'database'  => getenv('DB_DATABASE'),
-    'username'  => getenv('DB_USERNAME'),
-    'password'  => getenv('DB_PASSWORD'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
