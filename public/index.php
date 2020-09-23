9fcef7b965df436b104300c14f7413681f707602
altman.nicolas44@gmail.com <altman.nicolas44@gmail.com>
2020-09-23 16:01:28 -0300

    Set the display error configuration to see if something happen in heroku app

--- public/index.php
+++ public/index.php
@@ -3,9 +3,9 @@
 
 // Mostrar en pantalla siempre que ocurran errores
 // esto es para developing, no para production
-// ini_set('display_errros', 1); // inicializa variables de php para mostrar los errores
-// ini_set('display_starup_errror', 1); // encender los errores
-// error_reporting(E_ALL); // E_ALL: todos los errores
+ini_set('display_errros', 1); // inicializa variables de php para mostrar los errores
+ini_set('display_starup_errror', 1); // encender los errores
+error_reporting(E_ALL); // E_ALL: todos los errores
 
 const BASE_ROUTE = '';
 
