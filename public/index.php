a8e8bb08a51f2bafb97ae2a309b6e7e4d7b4c290
altman.nicolas44@gmail.com <altman.nicolas44@gmail.com>
2020-09-24 10:22:21 -0300

    Modified environment variables, if getenv() can't fetch data, it uses

--- public/index.php
+++ public/index.php
@@ -11,40 +11,55 @@ require_once '../vendor/autoload.php';
 
 session_start();
 
-// if (getenv('APP_ENV') !== 'production') {
-//     $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__.'/..');
-//     $dotenv->load();
-// }
+$db_driver = '';
+$db_host = '';
+$db_database = '';
+$db_username = '';
+$db_password = '';
+
+try {
+    $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__.'/..');
+    $dotenv->load();
+    $db_driver = getenv('DB_DRIVER');
+    $db_host = getenv('DB_HOST');
+    $db_database = getenv('DB_DATABASE');
+    $db_username = getenv('DB_USERNAME');
+    $db_password = getenv('DB_PASSWORD');
+} catch (Exception $err) {
+    $db_driver = $_SERVER['DB_DRIVER'];
+    $db_host = $_SERVER['DB_HOST'];
+    $db_database = $_SERVER['DB_DATABASE'];
+    $db_username = $_SERVER['DB_USERNAME'];
+    $db_password = $_SERVER['DB_PASSWORD'];
+}
+
+var_dump(getenv('DB_DRIVER'));
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-echo '$_SERVER["DB_DRIVER"]';
-var_dump($_SERVER['DB_DRIVER']);
-var_dump(phpinfo());
-
 $capsule = new Capsule;
-// $capsule->addConnection([
-//     'driver'    => getenv('DB_DRIVER'),
-//     'host'      => getenv('DB_HOST'),
-//     'database'  => getenv('DB_NAME'),
-//     'username'  => getenv('DB_USER'),
-//     'password'  => getenv('DB_PASS'),
-//     'charset'   => 'utf8',
-//     'collation' => 'utf8_unicode_ci',
-//     'prefix'    => '',
-//     'sslmode' => 'require'
-// ]);
 $capsule->addConnection([
-    'driver'    => 'pgsql',
-    'host'      => 'ec2-23-20-168-40.compute-1.amazonaws.com',
-    'database'  => 'd3m5ebrg7il9b8',
-    'username'  => 'kkjmgsrbflcmib',
-    'password'  => '00b2f3def22567084024d60724c07ff417b6a0f7969b4dd740f1ec0343a8d80e',
+    'driver'    => $db_driver,
+    'host'      => $db_host,
+    'database'  => $db_database,
+    'username'  => $db_username,
+    'password'  => $db_password,
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
     'sslmode' => 'require'
 ]);
+// $capsule->addConnection([
+//     'driver'    => 'pgsql',
+//     'host'      => 'ec2-23-20-168-40.compute-1.amazonaws.com',
+//     'database'  => 'd3m5ebrg7il9b8',
+//     'username'  => 'kkjmgsrbflcmib',
+//     'password'  => '00b2f3def22567084024d60724c07ff417b6a0f7969b4dd740f1ec0343a8d80e',
+//     'charset'   => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix'    => '',
+//     'sslmode' => 'require'
+// ]);
 
 // Make this Capsule instance available globally via static methods... (optional)
 $capsule->setAsGlobal();
